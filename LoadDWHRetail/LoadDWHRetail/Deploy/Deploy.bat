b99cb524420f532120d0a21131cdf41543b55dfa
Nat <sqlnat@gmail.com>
2018-04-26 10:20:04 +0100

    Deploy Script Updated

--- LoadDWHRetail/LoadDWHRetail/Deploy/Deploy.bat
+++ LoadDWHRetail/LoadDWHRetail/Deploy/Deploy.bat
@@ -1,3 +1,3 @@
-"C:\Program Files (x86)\Microsoft SQL Server\140\DAC\bin\SQlPackage.exe" /Action:Publish /SourceFile:..\bin\Debug\LoadDWHRetail.dacpac /TargetDatabaseName:ADV_DEV /TargetServerName:"localhost"
+"C:\Program Files (x86)\Microsoft SQL Server\140\DAC\bin\SQlPackage.exe" /Action:Publish /SourceFile:.LoadDWHRetail\LoadDWHRetail\bin\Debug\LoadDWHRetail.dacpac /TargetDatabaseName:ADV_DEV /TargetServerName:"localhost"
 pause
 
