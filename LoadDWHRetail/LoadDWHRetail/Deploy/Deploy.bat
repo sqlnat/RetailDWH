de4395942817f2cc69d178fbb92bd2749b1eabdd
Nat <sqlnat@gmail.com>
2018-04-26 09:39:30 +0100

    Added Deploy script

---
+++ LoadDWHRetail/LoadDWHRetail/Deploy/Deploy.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\Microsoft SQL Server\140\DAC\bin\SQlPackage.exe" /Action:Publish /SourceFile:..\bin\Debug\LoadDWHRetail.dacpac /TargetDatabaseName:ADV_DEV /TargetServerName:"localhost"
+pause
+
--- LoadDWHRetail/LoadDWHRetail/DimCurrency.sql
+++ LoadDWHRetail/LoadDWHRetail/DimCurrency.sql
@@ -3,6 +3,7 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [CurrecnyCode] NCHAR(10) NULL, 
     [Description] NCHAR(10) NULL,
-	CurrencyName nchar(50) null
+	CurrencyName nchar(50) null,
+	Remarks nvarchar(100)
 
 )
